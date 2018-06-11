ccc689e1f3235b86d51558568d2556860a14f781
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-06-11 11:02:03 +0700

    added loading

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -1,29 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Spinner = styled.span`
-	@keyframes spin {
-		0%,
-		100% {
-			border-radius: 50%;
-			transform: scale(1);
-		}
-		50% {
-			border-radius: 8px;
-			transform: scale(3);
-		}
-	}
+const FullScreenBox = styled.div`
 	position: fixed;
-	top: 50%;
-	left: 45%;
-	width: 10vw;
-	height: 10vw;
-	background-color: rgba(0, 0, 0, 0.3);
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	z-index: 5;
-	animation: spin 1s infinite ease-in-out;
+	
+`;
+
+const Spinner = styled.span`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+	min-width: 64px;
+	min-height: 64px;
+	padding: 2em;
+	font-weight:bold;
+	&::after {
+		content: '';
+		@keyframes spin {
+			0%,
+			100% {
+				border-radius: 50%;
+				transform: scale(1);
+			}
+			50% {
+				border-radius: 8px;
+				transform: scale(2);
+			}
+		}
+
+		
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: rgba(0, 0, 0, 0.3);
+		animation: spin 1s infinite ease-in-out;
+	}
 `;
 const LoadingSpinner = ({ isDisplay = false }) => {
-	return isDisplay && <Spinner />;
+	return (
+		isDisplay && (
+			<FullScreenBox>
+				<Spinner>LOADING...</Spinner>
+			</FullScreenBox>
+		)
+	);
 };
 
 export default LoadingSpinner;
